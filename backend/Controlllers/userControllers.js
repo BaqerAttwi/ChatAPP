e9c28141a91ff044ae093b62573bcf86b6fe6eb5
BaqerAttwi <mhmd.baqer.attwi@gmail.com>
2024-09-01 20:16:39 +0300

    Create routes,modules,userschema

---
+++ backend/Controlllers/userControllers.js
@@ -0,0 +1,6 @@
+const userModule=require("../Modules/userModule");
+
+const registerUser=(req,res)=>{
+    res.send("register");
+}
+module.exports={registerUser};
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express =require("express");
 const cors=require("cors");//require to communicate with the front end
 const mangoose = require("mongoose");
+const userRoute=require("./Routes/userRoute");
 const app=express();
 require("dotenv").config()
 app.use(express.json());//To use use json data
-app.use(cors());//as line 2 
+app.use(cors());//as line 2
+app.use("/api/users",userRoute); 
+app.get("/",(req,res)=>{
+  res.send("Welcome to our ChatAPP API's")
+})
 const port =process.env.port || 5000;
 const uri=process.env.AtLAS_URI;
 app.listen(port,(req , res)=>{
